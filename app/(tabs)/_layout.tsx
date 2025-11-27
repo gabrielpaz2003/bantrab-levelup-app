6ad61765d20ab99678d9c5608076128d6c0ca177
Roberto Rios <87707371+robertriosm@users.noreply.github.com>
2025-11-26 20:19:45 -0600

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,42 +4,48 @@ import { Colors, typography } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <SafeAreaProvider>
-      <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-          headerShown: false,
-          tabBarButton: HapticTab,
+      <SafeAreaView 
+        style={{ flex: 1, backgroundColor: Colors[colorScheme ?? 'light'].background }}
+        edges={["top", "left", "right"]}
+      >
+        <Tabs
+          screenOptions={{
+            tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+            headerShown: false,
+            tabBarButton: HapticTab,
+            
 
-        }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Roadmap',
-          tabBarIcon: ({ color }) => <IconSymbol size={typography.title.fontSize} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Estadísticas',
-          tabBarIcon: ({ color }) => <IconSymbol size={typography.title.fontSize} name="chart.bar.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="perfil"
-        options={{
-          title: 'Perfil',
-          tabBarIcon: ({ color }) => <IconSymbol size={typography.title.fontSize} name="person.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+          }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Roadmap',
+            tabBarIcon: ({ color }) => <IconSymbol size={typography.title.fontSize} name="house.fill" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="explore"
+          options={{
+            title: 'Estadísticas',
+            tabBarIcon: ({ color }) => <IconSymbol size={typography.title.fontSize} name="chart.bar.fill" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="perfil"
+          options={{
+            title: 'Perfil',
+            tabBarIcon: ({ color }) => <IconSymbol size={typography.title.fontSize} name="person.fill" color={color} />,
+          }}
+        />
+      </Tabs>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
